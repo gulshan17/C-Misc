5a0ad9533166a33bed58f456d42b60ff7fd447aa
Gulshan Kumar <gulshan1785@gmail.com>
2017-03-09 12:42:04 +0530

    Create optimisedarraylrotaion2.c

---
+++ optimisedarraylrotaion2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() { 
+    
+    int n,r;
+    scanf("%d %d",&n,&r);
+    int a[n],b[n];
+    for(int i=0;i<n;i++)
+        {
+           scanf("%d",&a[i]);
+    }
+    
+    for(int i=r;i<n;i++)
+        {
+           b[i-r]=a[i];
+    }
+    for(int i=0;i<r;i++)
+        {
+            b[n-r+i]=a[i];
+    }
+    for(int i=0;i<n;i++)
+        {
+           printf("%d ",b[i]);
+    }
+    
+    return 0;
+}
